67b90363dfb49bf052a8a90f1996b6b69e6dd49e
DeprezzedClutch <110886482+DeprezzedClutch@users.noreply.github.com>
2022-08-09 02:21:24 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0xA4BBeEBE5cdaa577249E613BC0EB7CDc8e6D598D",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "56",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Block Riders Genesis Vehicles",
 
     // 4. Set your Covalent API Key
     "api_key": "ckey_3bdabb7325ee47cc9bcc1e5f3d7",
@@ -16,7 +16,7 @@ export const CONFIG = {
     "timeseries_chart": true,
 
     // 6. (Optional) Set your banner image
-    "banner_picture": "",
+    "banner_picture": "https://img.tofunft.com/v2/56/0xa4bbeebe5cdaa577249e613bc0eb7cdc8e6d598d/265/720/image.jpg",
   },
 
   // Supported networks - just for your reference
